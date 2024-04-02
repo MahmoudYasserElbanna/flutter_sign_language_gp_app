0a5bc7f23c5886da7fb511caccca130e2f361cf2
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-04-02 12:40:14 +0200

    add speech 2 text model

---
+++ lib/models/speech2text_model.dart
@@ -0,0 +1,7 @@
+class Speech2Text {
+  final String speech2text;
+  Speech2Text({required this.speech2text});
+  factory Speech2Text.fromJson(jsonData) {
+    return Speech2Text(speech2text: '');
+  }
+}
