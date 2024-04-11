0ac0892529e95c9a1d5fd0bd027e456bee373d5a
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-04-11 10:57:38 +0200

    add firebase decencies

--- lib/models/speech2text_model.dart
+++ lib/models/speech2text_model.dart
@@ -2,6 +2,6 @@ class Speech2Text {
   final String speech2text;
   Speech2Text({required this.speech2text});
   factory Speech2Text.fromJson(jsonData) {
-    return Speech2Text(speech2text: '');
+    return Speech2Text(speech2text: jsonData['']);
   }
 }
