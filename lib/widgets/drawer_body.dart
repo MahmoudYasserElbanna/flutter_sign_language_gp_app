b9c5ae3bae6a0bf36c8b94678bd99bb2e0d738e6
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-04-11 15:47:10 +0200

    add about view

--- lib/widgets/drawer_body.dart
+++ lib/widgets/drawer_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:sign_language_gp_app/Views/dictionary_view.dart';
+import 'package:sign_language_gp_app/Views/learn_view.dart';
 import 'package:sign_language_gp_app/Views/setting_view.dart';
 import 'package:sign_language_gp_app/widgets/custom_list_title.dart';
 
@@ -19,7 +20,7 @@ class DrawerBody extends StatelessWidget {
         const Divider(color: Colors.white),
         CustomListTitle(
           title: 'التعليم',
-          pageId: SettingView.id,
+          pageId: LearnView.id,
           iconName: Icons.leaderboard,
         ),
         const Divider(color: Colors.white),
