404c476ed380a416412a16d5318e2f61cde131bd
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-04-12 15:23:24 +0200

    display image from fire store

--- lib/Views/landing_view.dart
+++ lib/Views/landing_view.dart
@@ -1,14 +1,39 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:sign_language_gp_app/constants.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:sign_language_gp_app/widgets/custom_text_filed.dart';
 import 'package:sign_language_gp_app/widgets/drawer_body.dart';
 
-class LandingView extends StatelessWidget {
+class LandingView extends StatefulWidget {
   LandingView({super.key});
   static String id = 'landingView';
-  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  @override
+  State<LandingView> createState() => _LandingViewState();
+}
+
+class _LandingViewState extends State<LandingView> {
+  final firestore = FirebaseStorage.instance;
+  late String imageUrl;
+  bool isLoading = true;
+
+  void initState() {
+    super.initState();
+    imageUrl = '';
+    getImageUrl();
+  }
+
+  Future<void> getImageUrl() async {
+    final ref = firestore.ref().child('Mohamed Ali Boxing Image.png');
+    final url = await ref.getDownloadURL();
+    setState(() {
+      imageUrl = url;
+      isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -23,14 +48,26 @@ class LandingView extends StatelessWidget {
           backgroundColor: kPrimaryColor,
           child: const DrawerBody(),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              // TODO : Firebase Logic to Retrive Vedio
-              CustomTextField(),
-            ],
+        body: Center(
+          child: ModalProgressHUD(
+            inAsyncCall: isLoading,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    height: 650.h,
+                    width: double.infinity,
+                    child: Image(
+                      image: NetworkImage(imageUrl),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  CustomTextField(),
+                ],
+              ),
+            ),
           ),
         ),
       ),
