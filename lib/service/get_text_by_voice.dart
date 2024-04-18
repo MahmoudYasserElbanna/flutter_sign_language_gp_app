73bd752a8617891c7ff03409b7e084108e100bb3
MahmoudYasserElbanna <mahmoudyelb2002@gmail.com>
2024-04-19 00:32:42 +0200

    handle Api Speech end point

--- lib/service/get_text_by_voice.dart
+++ lib/service/get_text_by_voice.dart
@@ -1,19 +1,29 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:sign_language_gp_app/helpers/api.dart';
 import 'package:sign_language_gp_app/models/speech2text_model.dart';
 
-// TODO This Class Need to Update
 class Speech {
   final Api api;
   Speech(this.api);
   Future<Speech2Text> speechToText({
-    required String text,
+    String text = "",
     required String voice_record,
   }) async {
-    Map<String, dynamic> data =
-        await api.post(postUrl: 'http://10.0.2.2:8000/models/speech', body: {
-      "text": text,
-      "voice_record": voice_record,
-    });
-    return Speech2Text.fromJson(data);
+    List<int> audioInBytes = await File(voice_record).readAsBytes();
+    String base64Audio = base64Encode(audioInBytes);
+
+    try {
+      Map<String, dynamic> data =
+          await api.post(postUrl: 'http://10.0.2.2:8000/models/speech', body: {
+        "text": text,
+        "voice_record": base64Audio,
+      });
+
+      return Speech2Text.fromJson(data);
+    } on Exception catch (e) {
+      print('Error in translation: $e');
+      rethrow;
+    }
   }
 }
